1c9a795a96fdb334c7d5b3995f5aded5c9e5b6f0
LightGive <roi.akase@outlook.jp>
2021-12-26 16:40:43 +0900

    Unity2020.3.22f1にアップグレード、UPM対応

---
+++ Runtime/Dither.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+namespace LightGive.PostProcessDither
+{
+    public class Dither : VolumeComponent, IPostProcessComponent
+    {
+        [Tooltip("4の倍数にして下さい")]
+        public MinIntParameter Width = new MinIntParameter(420, 4, true);
+        [Tooltip("4の倍数にして下さい")]
+        public MinIntParameter Height = new MinIntParameter(320, 4, true);
+
+        public TextureParameter DitherMatrixTexture = new TextureParameter(null, true);
+
+        public bool IsActive() => DitherMatrixTexture != null;
+        public bool IsTileCompatible() => false;
+
+        private void OnValidate()
+        {
+            if (Width.value % 4 != 0)
+                Width.value += Width.value % 4;
+            if (Height.value % 4 != 0)
+                Height.value += Height.value % 4;
+        }
+    }
+}
